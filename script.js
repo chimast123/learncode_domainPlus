a56ea0dee6e2114f10b67fca7f92cfab1938abc5
Godswill9 <ghycinth9@gmail.com>
2025-10-06 17:27:03 +0100

    calculator

---
+++ script.js
@@ -0,0 +1,54 @@
+let firstvalue = document.querySelector(".firstValue");
+let secondValue = document.querySelector(".secondValue");
+let sign = document.querySelector(".sign");
+let result = document.querySelector(".answer");
+let answerField = document.querySelector(".input-area input");
+let nextButton = document.querySelector(".input-area .next");
+let checkButton = document.querySelector(".input-area .checkBut");
+
+let signs = ["+", "-", "*", "/"];
+
+result.style.display = "none";
+
+firstvalue.innerHTML = Math.ceil(Math.random() * 100);
+secondValue.innerHTML = Math.ceil(Math.random() * 100);
+sign.innerHTML = signs[Math.floor(Math.random() * signs.length)];
+if (
+  Number(firstvalue.innerHTML) < Number(secondValue.innerHTML) &&
+  sign.innerHTML == "-"
+) {
+  firstvalue.innerHTML = Math.ceil(Math.random() * 100);
+  secondValue.innerHTML = Math.ceil(Math.random() * 100);
+}
+
+nextButton.addEventListener("click", () => {
+  result.style.display = "none";
+  firstvalue.innerHTML = Math.ceil(Math.random() * 100);
+  secondValue.innerHTML = Math.ceil(Math.random() * 100);
+  sign.innerHTML = signs[Math.floor(Math.random() * signs.length)];
+  if (
+    Number(firstvalue.innerHTML) < Number(secondValue.innerHTML) &&
+    sign.innerHTML == "-"
+  ) {
+    firstvalue.innerHTML = Math.ceil(Math.random() * 100);
+    secondValue.innerHTML = Math.ceil(Math.random() * 100);
+  }
+});
+
+checkButton.addEventListener("click", () => {
+  result.style.display = "block";
+  if (answerField.value == "") {
+    alert("Please enter a value");
+    return;
+  } else {
+    let finalAnswer = eval(
+      firstvalue.innerHTML + sign.innerHTML + secondValue.innerHTML
+    );
+    result.innerHTML = finalAnswer;
+    if (answerField.value !== String(finalAnswer)) {
+      alert("Wrong answer, view correct answer below!😉");
+    } else {
+      alert("Correct answer, well done!");
+    }
+  }
+});
